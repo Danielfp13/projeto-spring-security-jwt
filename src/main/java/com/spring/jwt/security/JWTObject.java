e5919330bb566db2f4c84c2816e8a32317a2fe71
Daniel Pereira <daniel.si@outlook.com.br>
2022-05-27 22:40:55 -0300

    Criando JWTObject classe que representa um objeto para gerar o token

---
+++ src/main/java/com/spring/jwt/security/JWTObject.java
@@ -0,0 +1,29 @@
+package com.spring.jwt.security;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+public class JWTObject {
+
+    private String subject; //nome do usuario
+
+    private Date issuedAt; //data de criação do token
+
+
+    private Date expiration; // data de expiração do token
+    private List<String> roles; //perfis de acesso
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+
+    public void setRoles(String... roles) {
+        this.roles = Arrays.asList(roles);
+    }
+}
